f4845aedc53f0b383d071bfe08f5a467aba3a14e
lenlen1rem <lissious@yandex.ru>
2026-03-15 21:35:38 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,20 +1,11 @@
+class URL:
+    main_url = 'https://stellarburgers.education-services.ru'
+
+
 class Endpoints:
-    #базовый URL API
-    base_url = 'https://stellarburgers.education-services.ru'
-    
-    #регистрация нового пользователя
-    register = f'{base_url}/api/auth/register'
-    #авторизация пользователя
-    login = f'{base_url}/api/auth/login'
-    #выход из системы
-    logout = f'{base_url}/api/auth/logout'
-    #обновление токена
-    refresh_token = f'{base_url}/api/auth/token'
-    #удаление пользователя
-    user_delete = f'{base_url}/api/auth/user'
-    #создание заказа
-    create_order = f'{base_url}/api/orders'
-    #получение заказов пользователя
-    user_orders = f'{base_url}/api/orders'
-    #получение ингредиентов
-    ingredients = f'{base_url}/api/ingredients'
+    CREATE_USER = '/api/auth/register'
+    DELETE_USER = '/api/auth/user'
+    LOGIN = '/api/auth/login'
+    CREATE_ORDER = '/api/orders'
+    GET_ORDERS = '/api/orders'
+    CHANGE_DATA = '/api/auth/user'
